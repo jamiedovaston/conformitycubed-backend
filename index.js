25bfe7299a1fb49b76b01ed75e6168f8e939d2b9
devdamo <jamiedovaston@outlook.com>
2026-03-07 00:51:28 +0000

    Created steam auth.

---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(session({ 
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get('/ping', (req, res) => {
+    res.status(200).json({
+        message: 'pong',
+    });
+});
+
+app.post('/auth', async (req, res) => {
+    const { ticket } = req.body;
+
+    if(!ticket) return res.status(400).json({ error: 'No ticket provided!' });
+
+    try {
+        const params = new URLSearchParams({
+            key: process.env.STEAM_PUBLISHER_KEY,
+            appid: process.env.STEAM_APP_ID,
+            ticket: ticket,
+            identity: process.env.STEAM_IDENTITY
+        });
+
+        const steamRes = await fetch(`https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/?${params}`);
+
+        if (!steamRes.ok) {
+            const text = await steamRes.text();
+            console.error("Steam API returned:", text);
+            return res.status(steamRes.status).json({ error: text });
+        }
+
+        const data = await steamRes.json();
+
+        if (!data?.response?.params?.steamid)
+            return res.status(401).json({ error: 'Invalid Steam ticket' });
+
+        const steamId = data.response.params.steamid;
+
+        req.session.steamId = steamId;
+
+        return res.json({
+            success: true,
+            steamId
+        });
+    }
+    catch(err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Steam authentication failed' });
+
+    }
+});
